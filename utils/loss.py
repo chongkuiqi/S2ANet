f66cbedeed96d20d8cc44069614578967da2a8e1
ckq <18810608559@163.com>
2022-05-28 14:48:55 +0800

    fix utils/loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -6,12 +6,6 @@ Loss functions
 import torch
 import torch.nn as nn
 
-from utils.metrics import bbox_iou
-from utils.torch_utils import de_parallel
-
-from copy import deepcopy
-
-from utils.metrics import bbox_iou_rotated
 
 def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
     # return positive, negative label smoothing BCE targets
